81727d955cad15db1bf3ef625395b768061bd550
kartik727 <kartikchoudhary9@gmail.com>
2021-08-12 00:11:21 +0530

    Added adaptive weights for flashcards

--- flashcards.py
+++ flashcards.py
@@ -1,14 +1,33 @@
+from numpy import dtype
 import pandas as pd
 import argparse
-
-LEARN_THRESHOLD = 5
+import json
+from namespace import Namespace
 
 class LearningOverException(Exception):
     pass
 
-def flashcards_(card: pd.Series, fc_df: pd.DataFrame, n_meanings: int) -> None:
-    print(card['Word'])
-    print(card['Type'])
+def update(df: pd.DataFrame, fc_df: pd.DataFrame) -> None:
+    for _, row in df.iterrows():
+        try:
+            _ = fc_df.loc[row.name]
+        except KeyError:
+            fc_row = {'count' : 0, 'current' : 0, 'status' : 0}
+            fc_df.loc[row.name] = fc_row
+
+def wt_func(val, weights):
+    if val==1:
+        return weights.good
+    elif val==0:
+        return weights.neutral
+    elif val==-1:
+        return weights.bad
+    else:
+        raise ValueError('Invalid status value, must be one of {-1, 0, 1}')
+
+def flashcards_(card: pd.Series, fc_df: pd.DataFrame, n_meanings: int, config: Namespace) -> None:
+    print(card.name[0])
+    print(card.name[1])
 
     try:
         correct = input('Press Enter to show meaning\n')
@@ -40,10 +59,7 @@ def flashcards_(card: pd.Series, fc_df: pd.DataFrame, n_meanings: int) -> None:
 
         correct = bool(correct)
 
-        try:
-            fc_row = fc_df.loc[(card['Word'], card['Type']), :]
-        except KeyError:
-            fc_row = {'count' : 0, 'current' : 0, 'status' : 0}
+        fc_row = fc_df.loc[card.name]
 
         fc_row['count'] += 1
         fc_row['current'] = fc_row['current'] + 1 if correct else 0
@@ -52,12 +68,12 @@ def flashcards_(card: pd.Series, fc_df: pd.DataFrame, n_meanings: int) -> None:
 
         if fc_row['current'] == 0:
             fc_row['status'] = -1
-        elif fc_row['current'] > LEARN_THRESHOLD:
+        elif fc_row['current'] > config.learn_threshold:
             fc_row['status'] = 1
         else:
             fc_row['status'] = 0
 
-        fc_df.loc[(card['Word'], card['Type']), :] = fc_row
+        fc_df.loc[card.name] = fc_row
 
     except KeyboardInterrupt:
         print('Cancelled.')
@@ -66,28 +82,35 @@ def flashcards_(card: pd.Series, fc_df: pd.DataFrame, n_meanings: int) -> None:
     finally:
         print('\n' + '-'*80 + '\n')
 
-def flashcards(df: pd.DataFrame, fc_df: pd.DataFrame) -> None:
-    n_meanings = len(df.columns) - 2
+def flashcards(df: pd.DataFrame, fc_df: pd.DataFrame, config: Namespace) -> None:
+    n_meanings = len(df.columns)
 
     try:
-        for idx, flashcard in df.iterrows():
-            flashcards_(flashcard, fc_df, n_meanings)
+        while True:
+            wts = fc_df['status'].apply(wt_func, weights=config.learn_weights)
+            idx = fc_df.sample(weights=wts).iloc[0]
+            flashcard = df.loc[idx.name]
+            flashcards_(flashcard, fc_df, n_meanings, config)
     except LearningOverException:
         print('Learning Over.')
 
 
-def main():
+def main() -> None:
     parser = argparse.ArgumentParser()
-    args = parser.parse_args()
+    args = parser.parse_args() # For future
 
     save_path = 'data/meanings.csv'
     data_path ='data/flashcard_data.csv'
+    config_path = 'data/config.json'
 
-    df = pd.read_csv(save_path)
-    df = df.sample(frac=1.)
-    fc_df = pd.read_csv(data_path, index_col=['Word', 'Type'])
+    with open(config_path, 'r') as f:
+        config = Namespace(**json.load(f))
 
-    flashcards(df, fc_df)
+    df = pd.read_csv(save_path, index_col=['Word', 'Type'])
+    fc_df = pd.read_csv(data_path, index_col=['Word', 'Type'], dtype={'count':int, 'current':int, 'status':int})
+
+    update(df, fc_df)
+    flashcards(df, fc_df, config)
 
     fc_df.to_csv(data_path)
 
---
+++ namespace.py
@@ -0,0 +1,25 @@
+class Namespace:
+    def __init__(self, **kwargs):
+        self.update(**kwargs)
+
+    def update(self, **kwargs):
+        for kw, arg in kwargs.items():
+            if type(arg)==dict:
+                setattr(self, kw, Namespace(**arg))
+            else:
+                setattr(self, kw, arg)
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __getitem__(self, index):
+        return list(self.__dict__.values())[index]
+
+    def get_from_list(self, arg_list):
+        ret_dict = {}
+        for arg in arg_list:
+            ret_dict[arg] = getattr(self, arg)
+        return  ret_dict
+
+    def get(self):
+        return self.get_from_list(self.__dict__.keys())
