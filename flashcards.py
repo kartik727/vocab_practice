982d230265cbe9d6c6aa396847508b555697b70e
kartik727 <kartikchoudhary9@gmail.com>
2021-09-11 23:26:41 +0530

    Changed threshold inequality

--- flashcards.py
+++ flashcards.py
@@ -102,7 +102,7 @@ def flashcards_(card: pd.Series, fc_df: pd.DataFrame, n_meanings: int, config: N
 
         if fc_row['current'] == 0:
             fc_row['status'] = -1
-        elif fc_row['current'] > config.learn_threshold:
+        elif fc_row['current'] >= config.learn_threshold:
             fc_row['status'] = 1
         else:
             fc_row['status'] = 0
@@ -174,7 +174,7 @@ def show_session_data(fc_df: pd.DataFrame) -> None:
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('--status', action='store_true', help='Give status of current progression')
-    parser.add_argument('-g', '--group_num', default=None, help='Option to practice a particular group')
+    parser.add_argument('-g', '--group_num', default=None, type=int, help='Option to practice a particular group')
     args = parser.parse_args()
 
     save_path = 'data/meanings.csv'
